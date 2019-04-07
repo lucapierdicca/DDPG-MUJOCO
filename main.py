cf30e1655189e488176e9ff08e0405ddef57ce24
lucapierdicca <luca.pierdicca@gmail.com>
2019-04-07 14:06:45 +0200

    first

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
 		'num_train_steps':50,
 		'replay_init_size':1000,
 		'batch_size':64,
-		'render_train':True,
+		'render_train':False,
 		'restore':False,
 		'env':'RoboschoolHopper-v1_kirkiles_train50episode_noise_norm_bufsize1Mi1k'
 	}
